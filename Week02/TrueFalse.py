c3d908e412db7b21bc34801385b9f89203c57060
DBrennan88 <162179697+DBrennan88@users.noreply.github.com>
2024-03-31 18:58:22 +0100

    Add files via upload

---
+++ Week02/TrueFalse.py
@@ -0,0 +1,18 @@
+#Week02
+#Programme  True/False if 2 variaables are equal 
+#Author Darragh Brennan
+
+result =True ==True   ##added these but turns out theu serve no purpose
+result =False ==False  ##added these but turns out theu serve no purpose
+
+   #result 2 = 3 ##invalid syntax
+   #print (result)##invalid syntax
+
+#result if (2==3) ##invalid syntax
+#print (result)##invalid syntax
+
+result = 2 ==3
+print (result)
+
+
+
---
+++ Week02/subtract.py
@@ -0,0 +1,8 @@
+
+#Week02
+#Programme to subtract 2 numbers
+#Author Darragh Brennan
+
+result = 21 - 4  
+print (result)
+
