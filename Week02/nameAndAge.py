8af0be5506ba6524a7d63a32c3a04d0c6a7a18c0
DBrennan88 <162179697+DBrennan88@users.noreply.github.com>
2024-03-31 17:35:51 +0100

    Add files via upload

---
+++ Week02/nameAndAge.py
@@ -0,0 +1,12 @@
+#Week02
+#Extra Programme  -  reads name and age
+#Author Darragh Brennan
+name = "Darragh"
+number = 35
+
+## Attempt 1 print ("Hello {name} n\your age is + {number}") ~~~ Incorrect output : Hello {name} n\your age is + {number}
+
+## Attempt 2 print (f"Hello" {name} + "your age is" {number}) ~~~ too many ""
+
+print (f"Hello {name}, your age is {number}") 
+
