cd789dc60486fbe459fbf5aaa67cc85d67a30197
DBrennan88 <162179697+DBrennan88@users.noreply.github.com>
2024-04-03 20:12:30 +0100

    Add files via upload

---
+++ Week02/##Week 2 Task.py
@@ -0,0 +1,17 @@
+##Week 2 Task
+##Programme to read in 2 sums as ints ; output in single sum as float
+##Author Darragh Brennan
+
+Centamount1 = int(65)  #SET INT VARIABLE1
+Centamount2 = int(180) #SET INT VARIABLE2
+
+NetAmount = Centamount1 + Centamount2  ## SET VARIABLE 3 TO EQUALY SUM OF 1 AND 2
+EURresult = NetAmount   / 100     ##VALRIABLE4  RESULT VALUE ASSIGNED ( / OPERATOR ADDED TO CONVERT CENT TO EUR)
+print (f"The sum of these is $ {EURresult}")    #PRINT VARIABLE 4 TOTAL EUR AMOUNT IN F string readable format
+                                                ## f string used to coombine text and variable in single output
+                                                ##  " " used for free test in string
+                                                ## {} used for variable to be called 
+
+
+
+
