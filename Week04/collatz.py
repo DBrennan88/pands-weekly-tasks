936ccd7673a7459497d2f5a2710614c0a41b38d6
DBrennan88 <162179697+DBrennan88@users.noreply.github.com>
2024-04-06 10:44:14 +0100

    Add files via upload

---
+++ Week04/collatz.py
@@ -0,0 +1,7 @@
+## Week 4 Task
+## Programme to to input any positive integer and outputs the successive values of the calculation. 
+## At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+## Have the program end if the current value is one.
+## Author Darragh Brennan
+
+
